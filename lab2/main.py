f00cc75a866efb91ed2df8ae583e67435b9db074
EzanShah0530 <ezanshah58@gmail.com>
2024-11-18 17:19:37 +0600

    Lab 2

--- lab2/main.py
+++ lab2/main.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # type: ignore
 import random
 from controller.PriceMonitoringController import pricemonitoringcontroller
 from controller.InventoryMonitoringController import inventorymonitorcontroller
